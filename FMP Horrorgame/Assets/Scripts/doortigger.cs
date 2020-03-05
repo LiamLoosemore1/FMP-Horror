ddd9e1dadbb2ac171de3608dcb912ed2e96a6d08
liam <474967@chrome.escg.ac.uk>
2020-03-05 16:12:32 +0000

    Added door an animation as well as trigger

---
+++ FMP Horrorgame/Assets/Scripts/doortigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doortigger : MonoBehaviour
+{
+
+    Animator Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Anim.SetTrigger("Door_Open");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Anim.enabled = true;    
+    }
+    void pauseAmimationEvent()
+    {
+        Anim.enabled = false;
+    }
+
+}
