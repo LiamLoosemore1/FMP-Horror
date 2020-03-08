5241fd6f9ad4f79d45802555a410e13ab9cd8c6e
leekiller1234 <liamloosemore2@gmail.com>
2020-03-08 22:19:35 +0000

    Added sounds w2ith trigger as well as a sound for the door when you press "e"

---
+++ FMP Horrorgame/Assets/Scripts/playSoundonE.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSoundonE : MonoBehaviour
+{
+    public AudioSource Doorsound;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            Doorsound.Play();
+        }    
+    }
+}
