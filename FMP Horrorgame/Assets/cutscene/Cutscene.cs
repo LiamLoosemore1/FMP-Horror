8c6f759f50af0903ac2dc484cc602da4b4934303
leekiller <474967@chrome.escg.ac.uk>
2020-03-12 11:09:27 +0000

    Deleting unused or unwanted assets

--- FMP Horrorgame/Assets/cutscene/Cutscene.cs
+++ FMP Horrorgame/Assets/cutscene/Cutscene.cs
@@ -7,11 +7,20 @@ public class Cutscene : MonoBehaviour
     public GameObject thePlayer;
     public GameObject cutsceneCam;
 
-    void OnTriggerEnter(Collider other)
+    void OnButtonPress.getkeyE(Collider other)
     {
         cutsceneCam.SetActive(true);
         thePlayer.SetActive(false);
+
+        IEnumerator endCutScene()
+        {
+            yield return new WaitForSeconds(5);
+            thePlayer.SetActive(true);
+            cutsceneCam.SetActive(false);
+        }
     }
+
+    
     
 
 }
